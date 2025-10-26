0e59bae8b3e7e600333ed7e46e18d15fc4ecb2c3
Your NameHappiness <you@example.comobochihappiness180@gmail.com>
2025-10-26 20:37:27 +0000

    setting up github actions for Testing

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -98,6 +98,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'messaging_app.wsgi.application'
 
 
+o
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 if os.getenv('USE_MYSQL', 'false').lower() == 'true':
